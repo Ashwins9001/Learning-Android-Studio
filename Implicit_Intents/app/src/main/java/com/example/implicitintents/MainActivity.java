a4ee9f6e0f2d8f2754202266bc2ea02e297ff33a
ashwins9001 <nitromekid@hotmail.com>
2020-02-05 23:04:53 +0530

    Implicit intent for location
    Used geo search query to open google maps upon implicit intent

--- Implicit_Intents/app/src/main/java/com/example/implicitintents/MainActivity.java
+++ Implicit_Intents/app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.implicitintents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,12 +13,15 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     //hold EditText obj for URI
     private EditText mWebsiteEditText;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebsiteEditText = findViewById(R.id.website_edittext);
+        mLocationEditText = findViewById(R.id.location_edittext);
+
     }
 
     public void openWebsite(View view) {
@@ -30,6 +34,20 @@ public class MainActivity extends AppCompatActivity {
         //Return activity component to handle event, multiple activities can be chosen from baesd
         //On priority
         if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else {
+            Log.d("ImplicitIntents", "Can't handle intent");
+        }
+    }
+
+    public void openLocation(View view) {
+        String loc = mLocationEditText.getText().toString();
+        //Added geo search query tag will automatically open maps if installed
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+        if(intent.resolveActivity(getPackageManager()) != null)
+        {
             startActivity(intent);
         }
         else {
