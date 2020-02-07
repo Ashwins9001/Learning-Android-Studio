2fc54e36b0c9a2750fedea86db5325481642a45d
ashwins9001 <nitromekid@hotmail.com>
2020-02-07 22:55:49 +0530

    shopping cart icon+activity

--- droid_cafe/app/src/main/java/com/example/droidcafe/MainActivity.java
+++ droid_cafe/app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.droidcafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -26,8 +27,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //explicit intent, specifically state OrderActivity will fulfill it
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                startActivity(intent);
             }
         });
     }
