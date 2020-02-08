f4fb4f48ecff42bdaa1bbb2ca1f17ef5e2deffe4
ashwins9001 <nitromekid@hotmail.com>
2020-02-08 16:29:00 +0530

    radio btn for delivery

--- droid_cafe/app/src/main/java/com/example/droidcafe/OrderActivity.java
+++ droid_cafe/app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -17,5 +20,31 @@ public class OrderActivity extends AppCompatActivity {
         String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textview = findViewById(R.id.order_textview);
         textview.setText(message);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        //confirm radio button is checked/clicked, cast view to RadioButton
+        boolean checked = ((RadioButton) view).isChecked();
+        //check which one was clicked by referencing view id
+        switch(view.getId()) {
+            case R.id.sameday:
+                if(checked)
+                    displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                if(checked)
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if(checked)
+                    displayToast(getString(R.string.pickup));
+                break;
+            default:
+                break;
+        }
+
+    }
+    public void displayToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
