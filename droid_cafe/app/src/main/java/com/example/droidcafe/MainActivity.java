93c2aea488e803690ceef219af8176e2a1ead623
ashwins9001 <nitromekid@hotmail.com>
2020-02-08 18:49:50 +0530

    config menu items

--- droid_cafe/app/src/main/java/com/example/droidcafe/MainActivity.java
+++ droid_cafe/app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_order) {
             return true;
         }
 
