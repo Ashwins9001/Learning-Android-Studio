d16ec00928b193cab5f3114e2ca078a87d5b08db
ashwins9001 <nitromekid@hotmail.com>
2020-02-12 16:25:31 +0530

    Room database config

---
+++ Learning_Android/Kotlin/TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.trackmysleepquality.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//Annotations in Android Studio extend activities and components at compile-time, thus cannot be final
+//Used to simplify repetitive boiler-plate code, however they are lifecycle-dependent
+//If you annotate views, cannot ref in onCreate() as they won't exist till start-up
+//Primary key in SQL an added col that uniquely identifies each row
+//Each value of nightId is unique
+//Autogenerate lets Room define IDs itself 
+
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight (
+        @PrimaryKey(autoGenerate = true)
+        var nightId: Long = 0L,
+
+        @ColumnInfo(name = "start_time_milli")
+        val startTimeMilli: Long = System.currentTimeMillis(),
+
+        @ColumnInfo(name = "end_time_milli")
+        var endTimeMilli: Long = startTimeMilli,
+
+        @ColumnInfo(name = "quality_rating")
+        var sleepQuality: Int = -1 //indicate no sleep data collected
+        )
+
+
