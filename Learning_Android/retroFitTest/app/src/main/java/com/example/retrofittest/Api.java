15374f879b277e6174ded30c2f55195928e7faa6
ashwins9001 <nitromekid@hotmail.com>
2020-02-14 15:13:58 +0530

    Retrofit to retrieve from API

---
+++ Learning_Android/retroFitTest/app/src/main/java/com/example/retrofittest/Api.java
@@ -0,0 +1,14 @@
+package com.example.retrofittest;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+//Define interface, Retrofit will autogenerate methods to retrieve posts from API
+//Define relative URL in interface
+public interface Api {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
---
+++ Learning_Android/retroFitTest/app/src/main/java/com/example/retrofittest/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.retrofittest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewResult = findViewById(R.id.text_view_result);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        Api api = retrofit.create(Api.class);
+
+        Call<List<Post>> call = api.getPosts();
+
+        //Cannot run calls to server on main thread, will freeze exec
+        //Run thread in background via RetroFit method
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if(!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+                List<Post> posts = response.body();
+
+                for(Post post : posts) {
+                    String content = "";
+                    content += "Description: " + post.getDescription() + "\n";
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+}
