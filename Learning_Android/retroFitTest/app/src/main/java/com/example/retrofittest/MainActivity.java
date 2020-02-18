964ceb5da4875385ca4f22bff61b46bbd2c60940
ashwins9001 <nitromekid@hotmail.com>
2020-02-19 02:54:47 +0530

    Simplified adapter to handle one string, fixed output

--- Learning_Android/retroFitTest/app/src/main/java/com/example/retrofittest/MainActivity.java
+++ Learning_Android/retroFitTest/app/src/main/java/com/example/retrofittest/MainActivity.java
@@ -27,6 +27,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 class appendStr {String x; appendStr(String s) {x = s;}}
+class checkList{String y; checkList(String s){y = s;}}
 
 
 
@@ -50,13 +51,20 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Routes> routeData;
     StringBuilder builder = new StringBuilder();
     appendStr stations = new appendStr("");
+    checkList check = new checkList("");
+    StringBuilder routesForStation = new StringBuilder();
+    StringBuilder name = new StringBuilder();
+    String name2="";
+
 
     ArrayList<String> allStationBuses = new ArrayList<>();
 
-    List<RouteList> rAllItems = new ArrayList<>();
+    List<String> forRecyclerView = new ArrayList<>();
+
+   // List<RouteList> rAllItems = new ArrayList<>();
 
 
-    static class RouteList{
+/*    static class RouteList{
         String allRoutes;
        // ArrayList<String> allBuses;
         String allBuses;
@@ -72,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         TextView routeName;
         TextView routeDescription;
     }
-
+*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -112,63 +120,52 @@ public class MainActivity extends AppCompatActivity {
                 for(int i = 0; i < data.size(); i++)
                 {
                     builder.append(data.get(i).getName() + "\n");
+                    name.append(data.get(i).getName());
                     routeData = new ArrayList<>(Arrays.asList(temp[i].getRoutes()));
                     appendRoutes(builder, routeData, data.get(i).getName());
+                    routesForStation.delete(0, routesForStation.length());
                 }
+                ArrayAdapter<String> routeAdapter = new ArrayAdapter<>(MainActivity.this, R.layout.item, R.id.routes, forRecyclerView);
+                routeGrid.setAdapter(routeAdapter);
+
                 Log.w("Output", builder.toString());
-                //textViewResult.setText(builder.toString());
-                callAdapter();
             }
             @Override
             public void onFailure(Call<JSONResponse> call, Throwable t) {
                 textViewResult.setText(t.getMessage());
             }
+
         });
+
+
     }
 
     public void appendRoutes(StringBuilder b, ArrayList<Routes> addRoute, String currentRoute)
     {
-        StringBuilder routesForStation = new StringBuilder();
-        for (int i = 0; i < addRoute.size(); i++) {
+        b.delete(0, b.length());
+        for (int i = 0; i < addRoute.size(); i++)
+        {
             b.append("Route number " + i + " : " + addRoute.get(i).getRoutename() + "\n");
             routesForStation.append("Route number " + i + " : " + addRoute.get(i).getRoutename() + "\n");
 
-            allStationBuses.add(addRoute.get(i).getRoutename());
+          //  allStationBuses.add(addRoute.get(i).getRoutename());
         }
+        String temp = currentRoute + "\n\n" + b.toString() + "\n";
+        check.y = temp;
+        forRecyclerView.add(check.y);
+        Log.w("Strings", check.y);
 
-        rAllItems.add(new RouteList(currentRoute, routesForStation.toString()));
+        //rAllItems.add(new RouteList(currentRoute, routesForStation.toString()));
         b.append("\n\n\n");
         stations.x += b.toString();
+
     }
-    public void callAdapter()
+    /*
+    public void logRoutes()
     {
-        ArrayAdapter<RouteList> routeAdapter = new ArrayAdapter<RouteList>(this, 0, rAllItems) {
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-
-                RouteList currentRoutes = rAllItems.get(position);
-
-                // Inflate only once
-                if(convertView == null) {
-                    convertView = getLayoutInflater()
-                            .inflate(R.layout.custom_item, null, false);
-                    ViewHolder viewHolder = new ViewHolder();
-                    viewHolder.routeName =
-                            (TextView)convertView.findViewById(R.id.route_name);
-                    viewHolder.routeDescription =
-                            (TextView)convertView.findViewById(R.id.route_description);
-
-                    // Store results of findViewById
-                    convertView.setTag(viewHolder);
-                }
-
-                TextView routesName = ((ViewHolder)convertView.getTag()).routeName;
-                TextView routesDesc = ((ViewHolder)convertView.getTag()).routeDescription;
-                routesName.setText(currentRoutes.allRoutes);
-                routesDesc.setText(currentRoutes.allBuses);
-
-                return convertView;
-            }
-        };
-    }
+        for(int i = 0; i < forRecyclerView.size(); i++)
+        {
+            Log.w("Route List", forRecyclerView.get(i));
+        }
+    }*/
 }
